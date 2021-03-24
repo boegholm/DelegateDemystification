c7631e6f80fc0ad9698493c6c83968075d4fc09d
Thomas Bøgholm <github.com@glirc.dk>
2021-03-24 13:29:27 +0100

    Added example

--- DelegateDemystification/Program.cs
+++ DelegateDemystification/Program.cs
@@ -10,13 +10,22 @@ namespace ConsoleApp23
         static void Foo()=> Console.WriteLine("foo");
         static void Main(string[] args)
         {
+            Console.WriteLine(subjectone == null);
             subjectone += Foo;
+            Console.WriteLine(subjectone == null);
+            subjectone -= Foo;
+            Console.WriteLine(subjectone == null);
 
+
+            subjectone += Foo;
             var othersubject = subjectone;
 
             othersubject += Bar;
+            Console.WriteLine("added bar");
             othersubject -= Bar;
-
+            Console.WriteLine("removed bar");
+            othersubject.Notify();
+            Console.WriteLine("---");
             subjectone.Notify();
             Console.WriteLine("------");
             othersubject.Notify();
